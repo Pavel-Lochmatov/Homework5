2956cadd04aecde5c41612074cb7ae99969915dd
Pavel-Lokhmatov <hlest.ivsvnyby@gmail.com>
2021-11-20 19:53:39 +0400

    add test for numers of boder and reformat code

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,12 +4,12 @@ import static java.lang.Math.sqrt;
 
 public class SQRService {
 
-    public int findCountSqrt(int minborder,int maxborder) {
+    public int findCountSqrt(int minborder, int maxborder) {
         int counter = 0;
         for (int i = 0; i <= maxborder; i++) {
-            double sqrminborder=sqrt(minborder);
-            if (i >=sqrminborder && i * i <= maxborder) {
-             counter=counter+1;
+            double sqrminborder = sqrt(minborder);
+            if (i >= sqrminborder && i * i <= maxborder) {
+                counter = counter + 1;
             }
         }
         return counter;
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,28 +1,40 @@
 package ru.netology.sqr;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQRServiceTest {
     @Test
-    public void shouldFindNumExampleFirst(){
+    public void shouldFindNumExampleFirst() {
         SQRService service = new SQRService();
         int excepted = 6;
-        int actual = service.findCountSqrt(10,99);
-        Assertions.assertEquals(excepted,actual);
+        int actual = service.findCountSqrt(10, 99);
+        Assertions.assertEquals(excepted, actual);
     }
+
     @Test
-    public void shouldFindNumExampleTwo(){
+    public void shouldFindNumExampleTwo() {
         SQRService service = new SQRService();
         int excepted = 3;
-        int actual = service.findCountSqrt(200,300);
-        Assertions.assertEquals(excepted,actual);
+        int actual = service.findCountSqrt(200, 300);
+        Assertions.assertEquals(excepted, actual);
     }
+
     @Test
-    public void shouldFindNumExampleThird(){
+    public void shouldFindNumExampleThird() {
         SQRService service = new SQRService();
         int excepted = 4;
-        int actual = service.findCountSqrt(0,15);
-        Assertions.assertEquals(excepted,actual);
+        int actual = service.findCountSqrt(0, 15);
+        Assertions.assertEquals(excepted, actual);
+    }
+
+    @Test
+    public void shouldFindNumersOfBorder() {
+        SQRService service = new SQRService();
+        int excepted = 2;
+        int actual = service.findCountSqrt(0, 1);
+        Assertions.assertEquals(excepted, actual);
     }
 }
